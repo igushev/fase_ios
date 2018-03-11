0998547138ae1c052a77bb4fecef3e276d03ef30
Aleksey Bidnyk <lantekko@yandex.ru>
2018-03-11 16:36:19 +0200

    Added element mapping service.

---
+++ Fase_iOS/Services/ElementMappingService.swift
@@ -0,0 +1,42 @@
+//
+//  ElementMappingService.swift
+//  TestJsonIOS
+//
+//  Created by Aleksey on 3/10/18.
+//  Copyright © 2018 Alexey Bidnyk. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class ElementMappingService {
+    static func mapElement(with map: Map) -> Element? {
+        let elementTypeString = map.JSON["__class__"] as? String
+        let elementType = ElementType(with: elementTypeString)
+        
+        switch elementType {
+        case .label:
+            return Label.init(map: map)
+            
+        case .text:
+            return Text.init(map: map)
+            
+        case .switchElement:
+            return Switch.init(map: map)
+            
+        case .select:
+            return Select.init(map: map)
+            
+        case .image:
+            return Image.init(map: map)
+            
+        case .button:
+            return Button.init(map: map)
+            
+        default:
+            return nil
+        }                
+    }
+    
+    
+}
