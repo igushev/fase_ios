ad58310c782d09b06e1fe3bf3954b5dbfbfdaa18
Aleksey Bidnyk <lantekko@yandex.ru>
2018-03-11 16:37:00 +0200

    Added help extensions.

---
+++ Fase_iOS/Utils/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  TestJsonIOS
+//
+//  Created by Alexey Bidnyk on 3/8/18.
+//  Copyright © 2018 Alexey Bidnyk. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+extension Data {
+    static func dataToJSON(data: Data) -> AnyObject? {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as JSON
+        } catch let error {
+            print("Data to json conversion error: \(error)")
+        }
+        return nil
+    }
+    
+    static func jsonToData(json: JSON) -> Data? {
+        do {
+            return try JSONSerialization.data(withJSONObject: json, options: JSONSerialization.WritingOptions.prettyPrinted) as Data
+        } catch let error {
+            print("Json to data conversion error: \(error)")
+        }
+        return nil;
+    }
+    
+    func toString() -> String? {
+        return String(data: self, encoding: .utf8)
+    }
+}
+
+
